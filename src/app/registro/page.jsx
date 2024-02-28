5adde8689af985d07395f79b6eee9a7ad83d4c68
fgabrielqr <fgabrielqr@gmail.com>
2024-02-28 00:27:50 -0300

    feat: :zap: Modificando layout registro

--- src/app/registro/page.jsx
+++ src/app/registro/page.jsx
@@ -25,63 +25,73 @@ const Registro = () => {
         <div>
 
             {/* <Header /> */}
-            <div className="bg-gray-900 flex flex-col items-center justify-center min-h-screen py-2">
-                <h1 className="text-5xl font-bold mb-4 text-blue-500">Inscreva-Se</h1>
-                <form
-                    // onSubmit={handleSubmit}
-                    className="w-full max-w-lg p-6 bg-white border border-gray-300 rounded-lg shadow-md"
-                >
-                    <div className="mb-4">
-                        <label htmlFor="name" className="block text-gray-700">
-                            Nome
-                        </label>
-                        <input
-                            type="text"
-                            name="name"
-                            id="name"
-                            // value={name}
-                            // onChange={(e) => setName(e.target.value)}
-                            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 mt-1 block"
-                            required
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block text-gray-700">
-                            E-mail
-                        </label>
-                        <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            // value={email}
-                            // onChange={(e) => setEmail(e.target.value)}
-                            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 mt-1 block"
-                            required
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="affiliation" className="block text-gray-700">
-                            Afiliação
-                        </label>
-                        <input
-                            type="text"
-                            name="affiliation"
-                            id="affiliation"
-                            // value={affiliation}
-                            // onChange={(e) => setAffiliation(e.target.value)}
-                            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 mt-1 block"
-                            required
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 font-bold py-2 px-4 rounded"
+            <div className="bg-gray-900 flex flex-col md:flex-row items-center justify-center min-h-screen py-2">
+                <div className="hidden md:block md:w-1/1">
+                    <img
+                        src="https://source.unsplash.com/random/800x800?climate+change,future"
+                        alt="Climate Change & Future"
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+                <div className="w-full md:w-1/2 px-6 text-center md:text-left">
+
+                    <h1 className="text-5xl font-bold mb-4 text-blue-500">Inscreva-Se</h1>
+                    <form
+                        // onSubmit={handleSubmit}
+                        className="w-full max-w-lg p-6 bg-white border border-gray-300 rounded-lg shadow-md"
                     >
-                        Register
-                    </button>
-                </form>
+                        <div className="mb-4">
+                            <label htmlFor="name" className="block text-gray-700">
+                                Nome
+                            </label>
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                // value={name}
+                                // onChange={(e) => setName(e.target.value)}
+                                className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 mt-1 block"
+                                required
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="email" className="block text-gray-700">
+                                E-mail
+                            </label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                // value={email}
+                                // onChange={(e) => setEmail(e.target.value)}
+                                className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 mt-1 block"
+                                required
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="affiliation" className="block text-gray-700">
+                                Afiliação
+                            </label>
+                            <input
+                                type="text"
+                                name="affiliation"
+                                id="affiliation"
+                                // value={affiliation}
+                                // onChange={(e) => setAffiliation(e.target.value)}
+                                className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 mt-1 block"
+                                required
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 font-bold py-2 px-4 rounded"
+                        >
+                            Register
+                        </button>
+                    </form>
+                </div>
+                {/* <Footer /> */}
             </div>
-            {/* <Footer /> */}
         </div>
     );
 };
